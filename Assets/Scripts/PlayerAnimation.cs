db0111ccc54c665baea9b45bb62de77fc0bd18c1
David-Kor <36980670+David-Kor@users.noreply.github.com>
2018-09-13 21:40:13 +0900

    [ 다윗 ] - 무료 에셋을 다운받아 임시 이미지 추가 - 플레이어 이동 및 바라보는 방향 적용

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Sprite frontSprites;
+    public Sprite backSprites;
+    public Sprite rightSprites;
+
+    private PlayerMove playerMove;
+    private SpriteRenderer playerSprite;
+    private Vector2 spriteDirect;
+
+    void Awake()
+    {
+        playerMove = GetComponent<PlayerMove>();
+        playerSprite = GetComponent<SpriteRenderer>();
+        spriteDirect = Vector2.down;
+    }
+
+    void Update()
+    {
+        if (spriteDirect != playerMove.spriteDirect)
+        {
+            spriteDirect = playerMove.spriteDirect;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+
+            if (spriteDirect == Vector2.down) { playerSprite.sprite = frontSprites; }
+            else if (spriteDirect == Vector2.up) { playerSprite.sprite = backSprites; }
+            else if (spriteDirect == Vector2.right) { playerSprite.sprite = rightSprites; }
+            else if (spriteDirect == Vector2.left)
+            {
+                playerSprite.sprite = rightSprites;
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 30.0f;
+    public Vector2 spriteDirect;  //바라보는 방향
+
+    private Vector2 moveDirect;  //움직이는 방향
+    private float axisX;
+    private float axisY;
+
+    void Start()
+    {
+        moveDirect = Vector2.zero;
+        spriteDirect = Vector2.down;
+    }
+
+    void Update()
+    {
+        if (CheckMoveKeyInput())    //이동키를 눌렀는지 확인
+        {
+            AxisToDirect();
+            MovePlayer();   //플레이어 이동
+        }
+
+        if (Input.GetKey("down"))
+        {
+            moveDirect = Vector2.down;
+            if (Input.GetKey("up")) { moveDirect = Vector2.zero; }
+
+            if (Input.GetKey("left")) { moveDirect += Vector2.left; }
+            if (Input.GetKey("right")) { moveDirect += Vector2.right; }
+        }
+        else if (Input.GetKey("up"))
+        {
+            moveDirect = Vector2.up;
+            if (Input.GetKey("down")) { moveDirect = Vector2.zero; }
+
+            if (Input.GetKey("left")) { moveDirect += Vector2.left; }
+            if (Input.GetKey("right")) { moveDirect += Vector2.right; }
+        }
+        else if (Input.GetKey("left"))
+        {
+            moveDirect = Vector2.left;
+            if (Input.GetKey("right")) { moveDirect = Vector2.zero; }
+
+            if (Input.GetKey("down")) { moveDirect += Vector2.down; }
+            if (Input.GetKey("up")) { moveDirect += Vector2.up; }
+        }
+        else if (Input.GetKey("right"))
+        {
+            moveDirect = Vector2.right;
+            if (Input.GetKey("left")) { moveDirect = Vector2.zero; }
+
+            if (Input.GetKey("down")) { moveDirect += Vector2.down; }
+            if (Input.GetKey("up")) { moveDirect += Vector2.up; }
+        }
+    }
+
+    bool CheckMoveKeyInput()
+    {
+        if (Input.GetKey("down")) { return true; }
+        if (Input.GetKey("up")) { return true; }
+        if (Input.GetKey("left")) { return true; }
+        if (Input.GetKey("right")) { return true; }
+
+        moveDirect = Vector2.zero;
+        return false;
+    }
+
+    void AxisToDirect()
+    {
+        axisX = Input.GetAxis("Horizontal");
+        axisY = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(axisX) > Mathf.Abs(axisY))
+        {
+            if (axisX > 0) { spriteDirect = Vector2.right; }
+            else { spriteDirect = Vector2.left; }
+        }
+        else if (Mathf.Abs(axisX) < Mathf.Abs(axisY))
+        {
+            if (axisY > 0) { spriteDirect = Vector2.up; }
+            else { spriteDirect = Vector2.down; }
+        }
+    }
+
+    void MovePlayer()
+    {
+        transform.Translate(moveDirect * speed * Time.deltaTime, Space.World);
+    }
+
+}
