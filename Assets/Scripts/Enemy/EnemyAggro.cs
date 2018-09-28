d8537e4f0579add8ee02cc4b07223b539af6c3a0
David-Kor <36980670+David-Kor@users.noreply.github.com>
2018-09-28 16:04:43 +0900

    [ 다윗 ] - 몬스터에게 공격성(Aggro) 속성 추가   -> 선제 공격 여부를 공격성으로 결정   -> 공격성이 0이하이면 비선제공격 몬스터
    - 일정 범위 내로 접근한 플레이어를 일정 확률로 선제 공격함
      -> 정해진 시간마다 선제 공격 여부 결정
      -> 거리가 가까워질수록 선제공격 확률 급격히 증가
      -> 공격성이 최소 1이상이라면 매우 근접할 경우 무조건 선제 공격 실시

--- Assets/Scripts/Enemy/EnemyAggro.cs
+++ Assets/Scripts/Enemy/EnemyAggro.cs
@@ -4,26 +4,60 @@ using UnityEngine;
 
 public class EnemyAggro : MonoBehaviour
 {
-    public bool isKOS;  //Killed On Sight -> 선공 여부 결정
+    public float aggroDelay;   //선공을 결정할 초 단위 시간(딜레이)
 
+    private float aggroTimer;   //딜레이까지의 시간을 재는 타이머
+    private int autoAggro;   //어그로 수치(공격성) -> 값이 높을 수록 선공할 확률이 오름
+    private int maxRandRange;   //랜덤 값의 최대 파라매터 -> 거리가 가까울 수록 낮아짐
+
+    //정확한 거리를 계산면 소수를 계산하며, sqrt연산까지 하면 보통 무한소수가 나옴
+    //연산시간 단축을 위해 int형을 사용하여 정확한 거리값 대신 일정 수치로 표현
+    private int distX;  //타겟과의 X값 거리
+    private int distY;  //타겟과의 Y값 거리
+
+    private EnemyStatus status;
     private EnemyMove enemyMove;
-    private GameObject curTarget;
-    private GameObject prevTarget;
+    private GameObject target;
+
 
     void Start()
     {
+        status = GetComponentInParent<EnemyStatus>();
         enemyMove = GetComponentInParent<EnemyMove>();
-        curTarget = null;
-        prevTarget = null;
+        target = null;
+        autoAggro = status.autoAggro;
+        aggroTimer = 0f;
+        maxRandRange = 1000;
     }
 
 
     void Update()
     {
-        if (prevTarget != curTarget)
+        if (target != null)
         {
-            prevTarget = curTarget;
-            enemyMove.MoveToThisObject(curTarget);
+            aggroTimer += Time.deltaTime;
+            //x의 거리 수치
+            distX = Mathf.RoundToInt((target.transform.position.x - transform.position.x) * (target.transform.position.x - transform.position.x) * 10);
+            //y의 거리 수치
+            distY = Mathf.RoundToInt((target.transform.position.y - transform.position.y) * (target.transform.position.y - transform.position.y) * 10);
+
+            //소수점이하 버림
+            maxRandRange = Mathf.FloorToInt(1000 - (1000 / ((distX + distY) * (distX + distY) + 1)));
+
+            if (aggroTimer >= aggroDelay)
+            {
+                aggroTimer = 0;
+                
+                if (Random.Range(1, maxRandRange) <= autoAggro)
+                {
+                    enemyMove.MoveToThisObject(target);
+                }
+            }
+        }
+        else
+        {
+            maxRandRange = 1000;
+            enemyMove.MoveToThisObject(target);
         }
     }
 
@@ -31,22 +65,24 @@ public class EnemyAggro : MonoBehaviour
     /* AggroRadius의 Circle Collider안에 플레이어 접 */
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (!isKOS) { return; }
+        if (autoAggro <= 0) { return; }
 
         if (col.transform.parent.name == "Player")
         {
-            curTarget = col.transform.parent.gameObject;
+            aggroTimer = 0;
+            target = col.transform.parent.gameObject;
         }
     }
 
 
     void OnTriggerExit2D(Collider2D col)
     {
-        if (!isKOS) { return; }
+        if (autoAggro <= 0) { return; }
 
         if (col.transform.parent.name == "Player")
         {
-            curTarget = null;
+            aggroTimer = 0;
+            target = null;
         }
     }
 }
--- Assets/Scripts/Enemy/EnemyStatus.cs
+++ Assets/Scripts/Enemy/EnemyStatus.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class EnemyStatus : ObjectStatus
 {
     public float attackRange;
+    public int autoAggro;   //어그로 수치(공격성) -> 값이 높을 수록 선공할 확률이 오름 (최댓값 : 1000)
 }
