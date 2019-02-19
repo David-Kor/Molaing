46aba117e7a4c1a235219c7fb36f140810918392
CSH8217\csh82 <tnrwl4@naver.com>
2019-02-19 17:02:10 +0900

    [지호] - TalkSystem 개선. 대화 내용을 한 글자씩 출력함.

--- Assets/Scripts/UI/Window/TalkSyetem.cs
+++ Assets/Scripts/UI/Window/TalkSyetem.cs
@@ -5,62 +5,54 @@ using UnityEngine.UI;
 
 public class TalkSyetem : MonoBehaviour
 {
-    public int talkCnt = 10;       // 대화의 총 갯수를 설정해줍니다.
+    List<string> talking = new List<string>();
     int strCnt = 0;         // 이 변수가 하나씩 커져가면서 대화를 진행합니다.
-    string[] talk;          // 대화 내용을 저장할 공간입니다.
-    Text txt;        // Text 오브젝트에 접근하기
-    int[] showCnt;
-    // Use this for initialization
+    Text talkingTxt;        // Text 오브젝트에 접근하기
+
     void Start()
     {
-        talk = new string[talkCnt]; // 대화 저장 공간을 초기화해줍니다.
-        txt = transform.GetChild(0).GetChild(1).GetComponent<Text>();
-        txt.text = null;
+        talkingTxt = transform.GetChild(0).GetChild(1).GetComponent<Text>();
+        talkingTxt.text = "";
         initialized();
 
-        StartCoroutine(CountDialog());
+        StartCoroutine(CountDialog());        
     }
+
     IEnumerator CountDialog()
     {
-        do
-        {
+        yield return StartCoroutine(ShowAll(talking[strCnt]));
+        strCnt++;
 
-            txt.text = "";
-            StartCoroutine(ShowAll(talk[strCnt]));
-            if (Input.GetKeyDown(KeyCode.Z))
-            {
-                txt.text = "";
-                strCnt++;
-                if (strCnt == talkCnt)
-                {
-                    IEnumerator Coroutine = CountDialog();
-                    StopCoroutine(Coroutine);
-                    gameObject.SetActive(false);
-                }
-                Debug.Log("대기중");
-            }
+        while (strCnt < talking.Count)
+        {
             yield return new WaitForSeconds(0.01f);
-        } while (true);
+            if(Input.GetKeyDown(KeyCode.Z))
+            {
+                yield return StartCoroutine(ShowAll(talking[strCnt]));
+                strCnt++;
+            }
+        }
+
+        gameObject.SetActive(false);
+        StopAllCoroutines();
     }
     IEnumerator ShowAll(string a)
     {
-        Debug.Log(a.Length);
-        if (!txt.text.Equals(a))
+        if (!talkingTxt.text.Equals(a))
         {
             for (int i = 0; i < a.Length; i++)
             {
-                txt.text += a[i];
-                yield return new WaitForSeconds(0.1f);
+                talkingTxt.text += a[i];
+                yield return new WaitForSeconds(0.02f);
             }
+            StopCoroutine("ShowAll");
         }
-        yield return null;
-        IEnumerator coroutine = ShowAll(a);
-        StopCoroutine(coroutine);
 
     }
     void initialized()
     {
-        talk[0] = "힝";
-        talk[1] = "히이이이이이이이이잉";
+        talking.Add("힝");
+        talking.Add("히이이이이이이이이잉");
+        talking.Add("");
     }
 }
